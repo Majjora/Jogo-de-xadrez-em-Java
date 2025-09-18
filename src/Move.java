062043f6e4abf2103de308408ceb3646ea4c26c7
Majjora <felps.curreia@gmail.com>
2025-09-18 15:49:33 -0300

    Atualizando o projeto com melhorias da IA

---
+++ src/Move.java
@@ -0,0 +1,10 @@
+// Move.java
+public class Move {
+    public final int fromR, fromC, toR, toC;
+    public Move(int fr, int fc, int tr, int tc) {
+        this.fromR = fr;
+        this.fromC = fc;
+        this.toR = tr;
+        this.toC = tc;
+    }
+}
